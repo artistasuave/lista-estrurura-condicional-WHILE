2dbd6f876f2a1ee8b2a3bdb13647612db16f0d8f
Michelle Rogério <themicaloo@gmail.com>
2018-04-24 11:20:26 -0300

    Exercicio01

---
+++ Exercicio01.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio01{
+
+    public static void main(String[]){
+        
+        String nome = " ";
+        
+        while(nome!=fim){
+        JOptionPane.showInputDialog(null, "Digite um nome");
+        }
+    }
+}
