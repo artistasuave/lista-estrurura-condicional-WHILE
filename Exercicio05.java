fb2ee943bb138482be80d4a0eb6ec314b8565884
Michelle Rogério <themicaloo@gmail.com>
2018-04-27 08:10:31 -0300

    05 e 06

--- Exercicio05.java
+++ Exercicio05.java
@@ -6,27 +6,25 @@ public class Exercicio05{
         
         double numero1 = Double.parseDouble(JOptionPane.showInputDialog("Insira um número"));
         double numero2 = Double.parseDouble(JOptionPane.showInputDialog("Insira mais um número"));
-        int opcaoSelecionada = 0;
+        String opcaoSelecionada = " ";
         
-        while(opcaoSelecionada != "5"){
-        opcaoSelecionada = (JOptionPane.showInputDialog(null,
-        "1 - SOMAR"+
-        "\n2 - SUBTRAIR"+
-        "\n3 - MULTIPLICAR"+
+        while(!opcaoSelecionada.equals("5")){
+        opcaoSelecionada = JOptionPane.showInputDialog(null,             
+        "1 - SOMAR"+       
+        "\n2 - SUBTRAIR"+     
+        "\n3 - MULTIPLICAR"+     
         "\n4 - DIVIDIR"+
         "\n5 - SAIR"
-        ));
-        
-        if(opcaoSelecionada == 1){
+        );
+         if(opcaoSelecionada.equals(1)){
             JOptionPane.showMessageDialog(null, "A soma dos números é: "+(numero1+numero2));
-        }else if(opcaoSelecionada == 2){
+        }else if(opcaoSelecionada.equals(2)){
             JOptionPane.showMessageDialog(null, "A subtração dos números é: "+(numero1-numero2));
-        }else if(opcaoSelecionada == 3){
+        }else if(opcaoSelecionada.equals(3)){
             JOptionPane.showMessageDialog(null, "A multiplicação dos números é: "+(numero1*numero2));
-        }else if(opcaoSelecionada ==4){
+        }else if(opcaoSelecionada.equals(4)){
             JOptionPane.showMessageDialog(null, "A divisão dos números é: "+(numero1/numero2));
         }
-
         }
     }
 }
